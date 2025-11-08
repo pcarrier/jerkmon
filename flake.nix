feab14479be9a5c0c21a04858124958ffc71df3b
Pierre Carrier <pc@rrier.fr>
2025-11-08 18:33:33 +0000

    flake.nix

--- flake.nix
+++ flake.nix
@@ -4,21 +4,23 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, flake-utils, nixpkgs }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      flake-utils,
+      nixpkgs,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = import nixpkgs { inherit system; };
       in
-        {
-          devShell = pkgs.mkShell {
-            buildInputs = with pkgs; [ go rustc cargo gcc ];
-          };
-          packages.analyze = pkgs.buildGoModule {
-            pname = "analyze";
-            version = "0.2.0";
-            src = ./analyze;
-            vendorHash = "sha256-iuzm1kBhkObTDcUaayNkAqiVkzB/rlVdi6TKAhmpyTc=";
-          };
-        }
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            cargo
+          ];
+        };
+      }
     );
 }
